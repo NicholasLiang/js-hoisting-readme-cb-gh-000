38b5e2f2d6d5a8f6452b1dc5c143f2d2e8e9bb55
Nicholas Liang <ssss021418@gmail.com>
2018-05-04 17:13:21 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,32 +1,45 @@
 function callMe() {
+<<<<<<< HEAD
   lyric = "maybe";
+=======
+  var lyric = "maybe";
+>>>>>>> aa7687df6e8f480015639f2241ceac07894ffa6d
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
+  
 }
 
 
 function crazy() {
+<<<<<<< HEAD
   thisIsCrazy();
   
   // fix the code in here:
   function thisIsCrazy(){
+=======
+  // fix the code in here:
+  var thisIsCrazy = function (){
+>>>>>>> aa7687df6e8f480015639f2241ceac07894ffa6d
     console.log("hey!!!")
   }
+  thisIsCrazy();
+
+  
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+  
 
   function sayMy() {
     name = "Kristin";
     console.log(name);
     var name = "Kristin";
   }
+  sayMy();
 }
